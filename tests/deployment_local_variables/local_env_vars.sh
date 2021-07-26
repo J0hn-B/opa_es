0c17bd5089f1b248836eb577d612a50dce3bdfb2
J0hn-B <connect-wsman@hotmail.com>
2021-07-27 00:41:04 +0100

    add local vars template

---
+++ tests/deployment_local_variables/local_env_vars.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+#
+# Shell Script Template
+# - Set Azure CLI env vars
+#
+
+# # #* Update the env vars with your Azure subscription and SPN values
+
+# # #* Rename the script from "local_env_vars.sh" ==> "local_env_vars.ignore.sh"
+
+# # # Azure CLI env vars
+ARM_TENANT_ID=""
+ARM_CLIENT_ID=""
+ARM_CLIENT_SECRET=""
+ARM_SUBSCRIPTION_ID=""
+
+export ARM_TENANT_ID
+export ARM_CLIENT_ID
+export ARM_CLIENT_SECRET
+export ARM_SUBSCRIPTION_ID
