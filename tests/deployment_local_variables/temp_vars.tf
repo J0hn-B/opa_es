919789e3b24d8524ca26e1fab02cbafdbda492e9
J0hn-B <connect-wsman@hotmail.com>
2021-07-26 21:54:47 +0100

    initial update

---
+++ tests/deployment_local_variables/temp_vars.tf
@@ -0,0 +1,15 @@
+# Sert the Terraform environment variables
+
+
+variable "root_id_1" { default = "myorg-1" }
+
+variable "root_id_2" { default = "myorg-2" }
+
+variable "root_id_3" { default = "myorg-3" }
+
+variable "root_name" { default = "My Organization" }
+
+variable "location" { default = "uksouth" }
+
+## tf vars
+#test
