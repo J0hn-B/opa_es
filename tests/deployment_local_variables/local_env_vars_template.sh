4ff87997068d69dcc4f24d88822f19fc76adce78
J0hn-B <connect-wsman@hotmail.com>
2021-07-27 17:29:05 +0100

    comment update

--- tests/deployment_local_variables/local_env_vars_template.sh
+++ tests/deployment_local_variables/local_env_vars_template.sh
@@ -10,7 +10,7 @@ set -e
 # # #* Update the env vars with your Azure subscription and SPN values
 
 # # #* Rename the script: "local_env_vars_template.sh" ==> "local_env_vars.ignore.sh"
-# # #* Renaming the script to: "local_env_vars.ignore.sh" will add it to the .gitignore list.
+# # #* "local_env_vars.ignore.sh" will be added to the .gitignore list.
 
 # # # Azure CLI env vars
 ARM_TENANT_ID=""
